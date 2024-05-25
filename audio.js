c242bbf0614e77fdb91d0d5a36b218fa73a41c52
DogoWugu <bvbzwdkl@gmail.com>
2024-05-25 10:04:19 +0700

    test

--- audio.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var backgroundMusic = document.getElementById('background-music');
-
-  backgroundMusic.volume = 0.5; // Set volume to 50% for background music
-
-});
